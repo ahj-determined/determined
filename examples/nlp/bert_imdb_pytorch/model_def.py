6f106e997bf490905261a849757353ccfe6874bc
Angela Jiang <ahj@determined.ai>
2020-12-09 09:48:00 -0800

    Update bert imdb example to match video

--- examples/nlp/bert_imdb_pytorch/model_def.py
+++ examples/nlp/bert_imdb_pytorch/model_def.py
@@ -1,40 +1,27 @@
-from typing import Dict, Sequence, Union
-import torch
-import torch.nn as nn
+#!/usr/bin/env python3
 
-from determined.pytorch import DataLoader, PyTorchTrial, PyTorchTrialContext, LRScheduler
-import data
-import constants
+#from absl import app, flags, logging
 
-import transformers
+#import sh
+
+import torch as th
+#import pytorch_lightning as pl
+
 import nlp
-from transformers import (
-    AdamW,
-    get_linear_schedule_with_warmup,
-)
-from transformers.data.processors.squad import SquadResult
-from transformers.data.metrics.squad_metrics import (
-    compute_predictions_logits,
-    squad_evaluate,
-)
+import transformers
 
-TorchData = Union[Dict[str, torch.Tensor], Sequence[torch.Tensor], torch.Tensor]
+from determined.pytorch import DataLoader, PyTorchTrial, PyTorchTrialContext
 
-
-class BertIMDBPyTorch(PyTorchTrial):
-    def __init__(self, context: PyTorchTrialContext):
+class IMDBSentimentClassifier(PyTorchTrial):
+    def __init__(self, context):
         self.context = context
-
-        self.model = self.context.wrap_model(
-                transformers.BertForSequenceClassification.from_pretrained("bert-base-uncased"))
-
-        self.loss = torch.nn.CrossEntropyLoss(reduction='none')
-
-        self.optimizer = self.context.wrap_optimizer(
-         torch.optim.SGD(
+        self.model = self.context.wrap_model(transformers.BertForSequenceClassification.from_pretrained("bert-base-uncased"))
+        self.loss = th.nn.CrossEntropyLoss(reduction='none')
+        self.optimizer = self.context.wrap_optimizer(th.optim.SGD(
             self.model.parameters(),
             lr=self.context.get_hparam("learning_rate"),
-            momentum=0.9)) 
+            momentum=0.9,
+        ))
 
         self.prepare_data()
 
@@ -43,24 +30,18 @@ class BertIMDBPyTorch(PyTorchTrial):
 
         def _tokenize(x):
             x['input_ids'] = tokenizer.batch_encode_plus(
-                    x['text'],
-                    max_length=32,
+                    x['text'], 
+                    max_length=32, 
                     pad_to_max_length=True)['input_ids']
             return x
 
         def _prepare_ds(split):
-            ds = nlp.load_dataset('imdb', split=f"{split}[:1%]")
+            ds = nlp.load_dataset('imdb', split=f'{split}[:100%]')
             ds = ds.map(_tokenize, batched=True)
             ds.set_format(type='torch', columns=['input_ids', 'label'])
             return ds
 
         self.train_ds, self.test_ds = map(_prepare_ds, ('train', 'test'))
-
-    def build_training_data_loader(self):
-        return DataLoader(self.train_ds, drop_last=True, batch_size=self.context.get_per_slot_batch_size())
-
-    def build_validation_data_loader(self):
-        return DataLoader(self.test_ds, drop_last=True, batch_size=self.context.get_per_slot_batch_size(),)
 
     def forward(self, input_ids):
         mask = (input_ids != 0).float()
@@ -72,11 +53,25 @@ class BertIMDBPyTorch(PyTorchTrial):
         loss = self.loss(logits, batch['label']).mean()
         self.context.backward(loss)
         self.context.step_optimizer(self.optimizer)
-        return {"loss": loss}
+        return {'loss': loss}
 
     def evaluate_batch(self, batch):
         logits = self.forward(batch['input_ids'])
         loss = self.loss(logits, batch['label'])
         acc = (logits.argmax(-1) == batch['label']).float()
-        return {"validation_accuracy": acc}
+        return {'validation_acc': acc}
+
+    def build_training_data_loader(self):
+        return DataLoader(
+                self.train_ds,
+                batch_size=self.context.get_per_slot_batch_size(),
+                drop_last=True,
+                )
+
+    def build_validation_data_loader(self):
+        return DataLoader(
+                self.test_ds,
+                batch_size=self.context.get_per_slot_batch_size(),
+                drop_last=True,
+                )
 
